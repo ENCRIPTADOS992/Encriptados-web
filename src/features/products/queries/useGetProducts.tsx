c0fdd42b3a4be31003602d3091f93fafb650eb1d
Santiago Ortiz <sortiz@ammper.com>
2025-06-04 03:23:01 -0500

    Correcciones de los ver mas y traducciones de los endpoints

--- src/features/products/queries/useGetProducts.tsx
+++ src/features/products/queries/useGetProducts.tsx
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllProducts } from "../services";
 import { Allproducts } from "../types/AllProductsResponse";
+import { useLocale } from "next-intl";
 
 export const useGetProducts = (categoryId: number) => {
+  const locale = useLocale();
+
   return useQuery<Allproducts>({
     queryKey: ["products", categoryId],
-    queryFn: () => getAllProducts(categoryId),
+    queryFn: () => getAllProducts(categoryId, locale),
   });
 };
