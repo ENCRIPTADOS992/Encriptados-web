a4d204aa3fb5f90eea2e0b61c80410e8001d5781
Santiago Ortiz <sortiz@ammper.com>
2025-06-11 02:20:02 -0500

    Correcciones de los componentes

--- src/app/api/cripto/cryptomus-process/route.ts
+++ src/app/api/cripto/cryptomus-process/route.ts
@@ -1,14 +1,12 @@
 // src/app/api/cripto/cryptomus-process/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 
-export const config = { api: { bodyParser: false } };
-
 export async function POST(req: NextRequest) {
   try {
     const apiUrl = process.env.API_CRYPTO_MUS!;
     const response = await fetch(apiUrl, {
       method: 'POST',
-      body: await req.arrayBuffer(),   
+      body: await req.arrayBuffer(), // correcto para bodies binarios
     });
     const data = await response.json();
     return NextResponse.json(data, { status: response.status });
