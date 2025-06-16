3e07f980937a7e8b5c24b248fd893c298151fb7b
Santiago Ortiz <sortiz@ammper.com>
2025-06-16 00:29:21 -0500

    Ajuste de pagos de tarjetas

--- src/shared/components/StripeProvider.tsx
+++ src/shared/components/StripeProvider.tsx
@@ -5,12 +5,18 @@ import React from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!
+);
 
 export const StripeProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
+  if (!process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY) {
+    console.warn("🚨 STRIPE PUBLIC KEY is not defined");
+  }
+  console.log("Stripe loaded:", stripePromise);
   return <Elements stripe={stripePromise}>{children}</Elements>;
 };
