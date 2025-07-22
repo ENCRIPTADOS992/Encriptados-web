8d429d7149019b773dcf586570abb3f371ab5659
Santiago Ortiz <sortiz@ammper.com>
2025-07-22 02:59:47 -0500

    Correcciones de la vista de securecrypt

--- src/app/[locale]/apps/secureCrypt/components/SecureCryptProduct.tsx
+++ src/app/[locale]/apps/secureCrypt/components/SecureCryptProduct.tsx
@@ -9,7 +9,7 @@ const SecureProduct = () => {
 
   return (
     <SectionWrapper className="w-full py-10 px-4 lg:px-10 bg-white">
-<div className="max-w-7xl mx-auto flex flex-col [@media(min-width:768px)]:flex-row items-start justify-between gap-10">
+<div className="max-w-7xl mx-auto flex flex-col sm:flex-row items-start justify-between gap-10">
         {/* Texto */}
   <div className="w-full md:w-1/2 lg:w-[455px] flex flex-col justify-center">
           <h2 className="text-[#0F172A] font-bold text-2xl lg:text-3xl mb-2">
@@ -40,11 +40,11 @@ const SecureProduct = () => {
           <hr className="mb-4" />
           <p className="text-xs text-[#64748B] mb-1">{t("pricePrefix")}</p>
           <p className="text-2xl font-bold text-[#0F172A] mb-5">{t("price")}</p>
-          <div className="flex flex-wrap gap-4 mb-6">
-            <button className="bg-black text-white px-6 py-2 rounded-full text-sm flex items-center gap-2 hover:opacity-90">
+          <div className="flex flex-wrap sm:flex-nowrap gap-4 mb-6">
+            <button className="bg-black text-white px-6 py-2 rounded-full text-sm flex items-center gap-2 whitespace-nowrap hover:opacity-90">
               {t("buttons.buyNow")} 🛒
             </button>
-            <button className="bg-[#E6F6FB] text-[#054D61] px-6 py-2 rounded-full text-sm flex items-center gap-2 hover:bg-[#d7f0f7]">
+            <button className="bg-[#E6F6FB] text-[#054D61] px-6 py-2 rounded-full text-sm flex items-center gap-2 whitespace-nowrap hover:bg-[#d7f0f7]">
               {t("buttons.chatSupport")} 💬
             </button>
           </div>
