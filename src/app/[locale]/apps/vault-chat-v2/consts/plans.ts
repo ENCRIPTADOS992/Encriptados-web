2772c7a37458c88659f882879aec594cd312eb5c
Santiago Ortiz <sortiz@ammper.com>
2025-09-21 23:38:39 -0500

    Creacion de la vusta de vaul chat

--- src/app/[locale]/apps/vault-chat-v2/consts/plans.ts
+++ src/app/[locale]/apps/vault-chat-v2/consts/plans.ts
@@ -1,4 +1,4 @@
 export const plans = [
-  { label: 'Licencia 3 meses', value: '3' },
-  { label: 'Licencia 6 meses', value: '6' },
+  { label: 'Licencia 6 meses', value: '3' },
+  { label: 'Licencia 12 meses', value: '6' },
 ];
