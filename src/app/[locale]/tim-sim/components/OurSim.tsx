b9ef32c9acaafe14831f77154cf9e7eb1839b364
Santiago Ortiz <sortiz@ammper.com>
2025-12-01 09:20:10 -0500

    Correcciones de las cards

--- src/app/[locale]/tim-sim/components/OurSim.tsx
+++ src/app/[locale]/tim-sim/components/OurSim.tsx
@@ -1,50 +1,55 @@
 import OurSimCard from "./svgs/OurSimCard";
 import AdsClickSvg from "/public/images/bne-sim/svg/ads_click.svg";
-import CellTowerSvg from "/public/images/bne-sim/svg/cell_tower.svg";
+import CellTowerSvg from "/public/images/bne-sim/svg/cell_tower.png";
 import Rotate_rightSvg from "/public/images/bne-sim/svg/rotate_right.svg";
-import SimCardSvg from "/public/images/bne-sim/svg/sim_card.svg";
+import SimCardSvg from "/public/images/bne-sim/svg/sim_card.png";
 import { useTranslations } from "next-intl";
 
 const OurSim: React.FC = () => {
   const t = useTranslations("BneSimPage");
   return (
-    <div className="flex flex-col lg:flex-row items-center gap-10 max-w-[1100px] mx-auto">
-      <div className="ls:w-1/2 space-y-4">
-        <h2 className="text-5xl font-bold">{t("whyChooseSim")}</h2>
-        <p className="font-semibold text-2xl">{t("anonTitle")}</p>
-        <p className="text-2xl">{t("anonDescription")}</p>
+    <div className="flex flex-col lg:flex-row items-center gap-6 max-w-[1100px] mx-auto">
+      <div className="w-[545px] space-y-[33px]">
+        <h2 className="text-[48px] font-bold">{t("whyChooseSim")}</h2>
+        <p className="font-semibold text-[28px]">{t("anonTitle")}</p>
+        <p className="text-[28px]">{t("anonDescription")}</p>
       </div>
 
       <div className="ls:w-1/2 grid grid-cols-1 md:grid-cols-2 gap-4">
+       {/* 1. Oscura */}
         <OurSimCard
-          bgColor="bg-custom-gradient-our-sim-black"
+          bgColor="bg-[linear-gradient(180deg,#010101_0%,#010101_70%,#019EFF_100%)]"
           textColor="text-white"
           icon={CellTowerSvg}
-          title={t("OurSimCard.title")}
-          description={t("OurSimCard.description")}
-          classCard="md:-translate-y-4 "
+          title={t('OurSimCard.title')}
+          description={t('OurSimCard.description')}
+          classCard="md:-translate-y-4"
         />
 
+
+        {/* 2. Clara */}
         <OurSimCard
-          bgColor="bg-custom-gradient-our-sim-blue"
+          bgColor="bg-[linear-gradient(180deg,#009DFF_0%,#009DFF_70%,#C0E5FC_100%)]"
           textColor="text-black"
           icon={AdsClickSvg}
           title={t("OurSimCard.title2")}
           description={t("OurSimCard.description2")}
-          classCard="md:translate-y-4 "
+          classCard="md:translate-y-4"
         />
 
+        {/* 3. Clara */}
         <OurSimCard
-          bgColor="bg-custom-gradient-our-sim-blue"
+          bgColor="bg-[linear-gradient(180deg,#009DFF_0%,#009DFF_70%,#C0E5FC_100%)]"
           textColor="text-black"
           icon={Rotate_rightSvg}
           title={t("OurSimCard.title3")}
           description={t("OurSimCard.description3")}
-          classCard="md:-translate-y-4 "
+          classCard="md:-translate-y-4"
         />
 
+        {/* 4. Oscura */}
         <OurSimCard
-          bgColor="bg-custom-gradient-our-sim-black"
+          bgColor="bg-[linear-gradient(180deg,#010101_0%,#010101_70%,#019EFF_100%)]"
           textColor="text-white"
           icon={SimCardSvg}
           title={t("OurSimCard.title4")}
--- src/app/[locale]/tim-sim/components/svgs/OurSimCard.tsx
+++ src/app/[locale]/tim-sim/components/svgs/OurSimCard.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 interface OurSimCardProps {
   bgColor: string;
   textColor: string;
-  icon: string;
+  icon: string | StaticImageData;
   title: string;
   description: string;
   classCard: string;
@@ -19,12 +19,14 @@ const OurSimCard: React.FC<OurSimCardProps> = ({
 }) => {
   return (
     <div
-      className={`${bgColor} ${textColor} ${classCard} p-10 rounded-3xl h-[300px]`}
+      className={`${bgColor} ${textColor} ${classCard} p-8 rounded-3xl w-[260px] min-h-[360px]`}
     >
-      <div className="space-y-2 h-full flex flex-col justify-between">
+      <div className="flex flex-col justify-between h-full max-w-[228px]">
         <Image src={icon} alt="icon" width={50} height={50} className="mb-10" />
-        <h2 className="font-bold text-2xl">{title}</h2>
-        <p className="text-sm mb-10">{description}</p>
+        <h2 className="font-bold text-[26px] leading-[26px]">{title}</h2>
+        <p className="text-[12px] mt-4 mb-10 break-words">
+          {description}
+        </p>
       </div>
     </div>
   );
