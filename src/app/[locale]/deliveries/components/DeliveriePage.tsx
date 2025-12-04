1baf407bc66f2fcf454223295f3ac366b8329644
Santiago Ortiz <sortiz@ammper.com>
2025-12-04 00:21:20 -0500

    Correccion de faqs deliveries

--- src/app/[locale]/deliveries/components/DeliveriePage.tsx
+++ src/app/[locale]/deliveries/components/DeliveriePage.tsx
@@ -9,7 +9,9 @@ import HowItWorksDeliveriesTablet from "./HowItWorksDeliveriesTablet";
 import HowItWorksDeliveriesMobile from "./HowItWorksDeliveriesMobile";
 import FeaturedProductsDeliveries from "./FeaturedProductsDeliveries";
 import ModalJoinUsDeliveries from "./ModalJoinUsDeliveries";
-import FAQDeliveries from "./FAQDeliveries";
+import FAQSection from "./FAQSection";
+import FAQSectionMobile from "./FAQSectionMobile";
+import FAQSectionTablet from "./FAQSectionTablet";
 import { JoinUsModalProvider } from "../context/JoinUsModalContext";
 import { BasicFormProvider } from "@/shared/components/BasicFormProvider";
 import LogoCarousel from "./LogoCarousel";
@@ -21,6 +23,25 @@ import ProductCarouselMobile from "./ProductCarouselMobile";
 import DownloadAppBanner from "./DownloadAppBanner";
 
 const DeliveriesPage = () => {
+  const Faqs = [
+    {
+      question:
+        "¿Qué es la Entrega Rápida de Encriptados?",
+      answer:
+        "Nord VPN Chat cifra tus mensajes de extremo a extremo para garantizar privacidad total. Solo tú y tus contactos pueden leer los mensajes.",
+    },
+    {
+      question:
+        "¿Qué productos puedo comprar con Entrega Rápida?",
+      answer:
+        "Está disponible para dispositivos iOS y Android compatibles con las versiones actuales de la App Store y Google Play.",
+    },
+    {
+      question: "¿Qué costo tiene la Entrega Rápida de Encriptados?",
+      answer:
+        "Por su seguridad avanzada, facilidad de uso y soporte 24/7, ideal para quienes valoran la privacidad.",
+    },
+  ];
   return (
     <>
       <BasicFormProvider>
@@ -40,8 +61,10 @@ const DeliveriesPage = () => {
           <ProductCarouselTablet/>
           <ProductCarouselMobile/>
           <LogoCarousel />
+          <FAQSection faqs={Faqs} />
+          <FAQSectionMobile faqs={Faqs} />
+          <FAQSectionTablet faqs={Faqs} />
           {/* <DownloadAppBanner/> */}
-          <FAQDeliveries />
         </JoinUsModalProvider>
       </BasicFormProvider>
     </>
